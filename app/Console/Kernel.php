a02a95e21d3dc9743c639c54570cd8c190aed1f9
Sedat <sedattug@outlook.com>
2023-09-27 07:37:22 +0300

    Job frequency changed to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('check:subscriptions')->everyMinute();
+        $schedule->command('check:subscriptions')->daily();
     }
 
     /**
